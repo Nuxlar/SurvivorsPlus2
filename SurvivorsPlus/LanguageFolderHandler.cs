bd7a435f638828ae928b0c304558dae1f5a0a416
Nux <138419122+Nuxlar@users.noreply.github.com>
2025-06-21 00:33:28 -0400

    init

--- SurvivorsPlus/LanguageFolderHandler.cs
+++ SurvivorsPlus/LanguageFolderHandler.cs
@@ -1,6 +1,6 @@
 using System.IO;
 
-namespace NewMod
+namespace SurvivorsPlus
 {
     static class LanguageFolderHandler
     {
--- SurvivorsPlus/Main.cs
+++ SurvivorsPlus/Main.cs
@@ -5,14 +5,14 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-namespace NewMod
+namespace SurvivorsPlus
 {
   [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
   public class Main : BaseUnityPlugin
   {
     public const string PluginGUID = PluginAuthor + "." + PluginName;
     public const string PluginAuthor = "Nuxlar";
-    public const string PluginName = "NewMod";
+    public const string PluginName = "SurvivorsPlus";
     public const string PluginVersion = "1.0.0";
 
     internal static Main Instance { get; private set; }
